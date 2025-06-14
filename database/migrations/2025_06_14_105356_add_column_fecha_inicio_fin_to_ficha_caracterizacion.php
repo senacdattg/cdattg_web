f3e686971a39c857e6e869151474629c08e7c235
Jeferson Alvarez <mymotre213@gmail.com>
2025-06-14 17:10:35 -0500

    feat: agrega tablas para conectar instructores con fichas de caracterización

--- database/migrations/2025_06_14_105356_add_column_fecha_inicio_fin_to_ficha_caracterizacion.php
+++ database/migrations/2025_06_14_105356_add_column_fecha_inicio_fin_to_ficha_caracterizacion.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::table('fichas_caracterizacion', function (Blueprint $table) {
             $table->date('fecha_inicio')->nullable()->after('ficha');
             $table->date('fecha_fin')->nullable()->after('fecha_inicio');
+            $table->integer('total_horas')->nullable()->after('fecha_fin');
             $table->foreignId('ambiente_id')->nullable()->after('fecha_fin')->constrained('ambientes');
             $table->foreignId('modalidad_formacion_id')->nullable()->after('ambiente_id')->constrained('modalidades_formacion');
             $table->foreignId('sede_id')->nullable()->after('modalidad_formacion_id')->constrained('sedes');
@@ -29,12 +30,15 @@ return new class extends Migration
         Schema::table('fichas_caracterizacion', function (Blueprint $table) {
             $table->dropColumn('fecha_inicio');
             $table->dropColumn('fecha_fin');
+            $table->dropColumn('total_horas');
             $table->dropForeign(['ambiente_id']);
             $table->dropForeign(['modalidad_formacion_id']);
             $table->dropColumn('ambiente_id');
             $table->dropColumn('modalidad_formacion_id');
             $table->dropForeign(['sede_id']);
             $table->dropColumn('sede_id');
+            $table->dropForeign(['jornada_id']);
+            $table->dropColumn('jornada_id');
         });
     }
 };
