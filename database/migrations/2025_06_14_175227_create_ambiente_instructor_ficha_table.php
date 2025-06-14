a12879d82426ed26157aee8fad78f8f9abe1e1c5
Jeferson Alvarez <mymotre213@gmail.com>
2025-06-14 18:10:25 -0500

    Creando tablas para el control de ambientes

---
+++ database/migrations/2025_06_14_175227_create_ambiente_instructor_ficha_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ambiente_instructor_ficha', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha_asignacion')->nullable();
+            $table->foreignId('ambiente_id')->constrained('ambientes');
+            $table->foreignId('instructor_ficha_id')->constrained('instructor_fichas_caracterizacion');
+            $table->foreignId('instructor_id')->constrained('instructors');
+            $table->time('hora_entrada')->nullable();
+            $table->time('hora_salida')->nullable();
+            $table->foreignId('jornada_id')->constrained('jornadas_formacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ambiente_instructor_ficha');
+    }
+};
---
+++ database/migrations/2025_06_14_180217_create_ambiente_ficha_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ambiente_ficha', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ambiente_id')->constrained('ambientes');
+            $table->foreignId('ficha_id')->constrained('fichas_caracterizacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ambiente_ficha');
+    }
+};
