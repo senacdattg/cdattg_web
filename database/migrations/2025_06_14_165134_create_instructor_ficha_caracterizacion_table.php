3f2fb256047a5a32eaa415546b77863f674eae1c
Jeferson Alvarez <mymotre213@gmail.com>
2025-06-14 17:11:45 -0500

    Agregando la tabla de la conexion de instructores con fichas

---
+++ database/migrations/2025_06_14_165134_create_instructor_ficha_caracterizacion_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('instructor_fichas_caracterizacion', function (Blueprint $table) {
+            $table->id();
+            $table->integer('total_horas_instructor')->nullable();
+            $table->foreignId('instructor_id')->constrained('instructores');
+            $table->foreignId('ficha_id')->constrained('fichas_caracterizacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('instructor_fichas_caracterizacion');
+    }
+};
---
+++ database/migrations/2025_06_14_170542_create_instructor_ficha_dias_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('instructor_ficha_dias', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('instructor_ficha_id')->constrained('instructor_fichas_caracterizacion');
+            $table->foreignId('dia_id')->constrained('dias_formacion');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('instructor_ficha_dias');
+    }
+};
