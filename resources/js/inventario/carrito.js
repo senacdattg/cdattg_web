87d32060b8800e20c2fecd8dfb31a18ff54955f7
DESKTOP-2Q8H8VU\User <deysonurrego159@gmail.com>
2025-08-16 22:13:24 -0500

    Carrito casi terminado

---
+++ resources/js/inventario/carrito.js
@@ -0,0 +1,2 @@
+// Este archivo se deja vacío intencionalmente, ya que la funcionalidad del modal se ha movido 
+// a resources/js/inventario/shared/modal-imagen.js.
--- resources/views/inventario/carrito/index.blade.php
+++ resources/views/inventario/carrito/index.blade.php
@@ -1,7 +1,7 @@
 {{-- filepath: resources/views/inventario/carrito/index.blade.php --}}
 @extends('adminlte::page')
 
-@vite(['resources/css/inventario/carrito.css', 'resources/js/inventario/carrito.js'])
+@vite(['resources/css/inventario/carrito.css', 'resources/css/inventario/shared/modal-imagen.css', 'resources/js/inventario/shared/modal-imagen.js'])
 
 @section('content')
 @php
@@ -21,7 +21,56 @@
                 'imagen' => 'img/inventario/imagen_default.png'
             ],
             'cantidad' => 1
-        ]
+        ],
+        (object)[
+            'id' => 3,
+            'producto' => (object)[
+                'producto' => 'Teclado',
+                'imagen' => 'img/inventario/imagen_default.png'
+            ],
+            'cantidad' => 1
+        ],
+        (object)[
+            'id' => 4,
+            'producto' => (object)[
+                'producto' => 'Teclado',
+                'imagen' => 'img/inventario/imagen_default.png'
+            ],
+            'cantidad' => 1
+        ],
+         (object)[
+            'id' => 1,
+            'producto' => (object)[
+                'producto' => 'Mouse inalámbrico',
+                'imagen' => 'img/inventario/imagen_default.png'
+            ],
+            'cantidad' => 2
+        ],
+        (object)[
+            'id' => 2,
+            'producto' => (object)[
+                'producto' => 'Teclado mecánico',
+                'imagen' => 'img/inventario/imagen_default.png'
+            ],
+            'cantidad' => 1
+        ],
+        (object)[
+            'id' => 3,
+            'producto' => (object)[
+                'producto' => 'Teclado',
+                'imagen' => 'img/inventario/imagen_default.png'
+            ],
+            'cantidad' => 1
+        ],
+        (object)[
+            'id' => 4,
+            'producto' => (object)[
+                'producto' => 'Teclado',
+                'imagen' => 'img/inventario/imagen_default.png'
+            ],
+            'cantidad' => 1
+        ],
+
     ];
 @endphp
 
@@ -41,10 +90,9 @@
                 @foreach($carrito as $item)
                 <tr>
                     <td>
-                        <img src="{{ asset($item->producto->imagen ?? 'img/inventario/default.png') }}" 
-                        alt="Foto" 
-                        style="width:50px; height:50px; border-radius:8px;">
-                    </td>
+                        <img src="{{ asset($item->producto->imagen ?? 'img/inventario/default.png') }}"
+                             alt="Foto"
+                             class="img img-expandable">
                     <td>{{ $item->producto->producto }}</td>
                     <td>{{ $item->cantidad }}</td>
                     <td>
@@ -60,22 +108,41 @@
         </table>
     </div>
 
-    <div class="div_pedido">
-        <div class="div_titulo">
-            <h2><i class="fas fa-receipt"></i> Pedido </h2>
-        </div>
-        <div class="div_producto_titulo">
-            <p><strong>Productos:</strong> {{ count($carrito) }}</p>
-        </div>
-        <form>
-            @csrf
-            <div class="div_btn">
-                <button type="submit" class="btn-pedido">
-                    <i class="fas fa-paper-plane"></i> Enviar 
-                </button>
+    
+    <div class="div_lateral">
+        <div class="div_pedido">
+            <div class="div_titulo">
+                <h2><i class="fas fa-receipt"></i> Pedido </h2>
             </div>
-        </form>
+            <div class="div_titulo">
+                <p><strong>Productos:</strong> {{ count($carrito) }}</p>
+            </div>
+            <form>
+                @csrf
+                <div class="div_btn">
+                    <button type="submit" class="btn-pedido">
+                        <i class="fas fa-paper-plane"></i> Enviar 
+                    </button>
+                </div>
+            </form>
+        </div>
+
+        <div class="resumen">
+            <div class="div_titulo">
+                <h2><i class="fas fa-list-alt"></i> Resumen </h2>
+            </div>
+            <ul>
+                @foreach($carrito as $item)
+                    <li>{{ $item->cantidad }} x {{ $item->producto->producto }}</li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 </div>
 
+<!-- Modal para imagen expandida -->
+<div id="modalImagen" class="modal-imagen">
+    <span class="cerrar">&times;</span>
+    <img class="modal-contenido" id="imgExpandida">
+</div>
 @endsection
