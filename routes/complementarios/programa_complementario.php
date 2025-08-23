b7fed4394818bf8be801dcb2929ed269c09e7327
Samuel <rene28609@gmail.com>
2025-08-23 09:32:36 -0500

    ruta para ver el programa complementario agregada

---
+++ routes/complementarios/programa_complementario.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComplementarioController;
+
+Route::get('/programas/{programa}', [ComplementarioController::class, 'verPrograma'])
+    ->name('programa_complementario.ver')
+    ->middleware('auth');
+
