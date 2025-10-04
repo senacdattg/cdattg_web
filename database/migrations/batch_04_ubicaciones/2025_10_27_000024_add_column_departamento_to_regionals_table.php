7fa72dcb0e99109e75ab37e1bcfc19cedc7356d3
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2025-10-03 20:09:13 -0500

    Arreglo en las migraciones en la tabla regionales

--- database/migrations/batch_04_ubicaciones/2025_10_27_000024_add_column_departamento_to_regionals_table.php
+++ database/migrations/batch_04_ubicaciones/2025_10_27_000024_add_column_departamento_to_regionals_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,9 +12,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('regionals', function (Blueprint $table) {
-            $table->foreignId('departamento_id')->after('nombre')->constrained('departamentos');
-        });
+        if (!Schema::hasColumn('regionals', 'departamento_id')) {
+            Schema::table('regionals', function (Blueprint $table) {
+                $table->unsignedBigInteger('departamento_id')->nullable()->after('nombre');
+            });
+        } else {
+            // Make sure it's nullable
+            Schema::table('regionals', function (Blueprint $table) {
+                $table->unsignedBigInteger('departamento_id')->nullable()->change();
+            });
+        }
+
+        // Update invalid departamento_id to null
+        DB::table('regionals')->whereNotIn('departamento_id', DB::table('departamentos')->pluck('id'))->update(['departamento_id' => null]);
+
+        if (!collect(DB::select("SHOW KEYS FROM regionals WHERE Column_name='departamento_id' AND Key_name LIKE '%foreign%'"))->isNotEmpty()) {
+            Schema::table('regionals', function (Blueprint $table) {
+                $table->foreign('departamento_id')->references('id')->on('departamentos');
+            });
+        }
     }
 
     /**
