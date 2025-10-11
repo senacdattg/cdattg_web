8c73ead97a4f558c627af33c9647a9b209e1dd42
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-10-11 14:03:53 -0500

    fix(route): corregi la autentencion para ver detalles en el endpoint publico programas complementarios

--- routes/complementarios/programa_complementario.php
+++ routes/complementarios/programa_complementario.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComplementarioController;
 
-Route::get('/programas/{id}', [ComplementarioController::class, 'verPrograma'])
-    ->name('programa_complementario.ver');
+// Route moved to public routes in web.php
 
