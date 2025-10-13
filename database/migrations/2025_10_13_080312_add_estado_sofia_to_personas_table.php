b630a198d8e30ae131629dacdc9030c0b0b5a9b0
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-10-13 08:49:58 -0500

    feat(aspirantes): agregue la columna estado_sofia en aspirantes y modifique la vista de ver aspirantes para que la mostrar correctamente

---
+++ database/migrations/2025_10_13_080312_add_estado_sofia_to_personas_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('personas', function (Blueprint $table) {
+            $table->tinyInteger('estado_sofia')->default(0)->after('status')->comment('0: No registrado, 1: Registrado, 2: Requiere cambio de cédula');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('personas', function (Blueprint $table) {
+            $table->dropColumn('estado_sofia');
+        });
+    }
+};
