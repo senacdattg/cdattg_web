d423cd975cf02f03d9554c9f45c6c4a6043bca82
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-10-14 08:53:06 -0500

    fix(routes): corregi error de autenticacion en middleware

--- app/Http/Controllers/ComplementarioController.php
+++ app/Http/Controllers/ComplementarioController.php
@@ -15,6 +15,8 @@ use App\Models\AspiranteComplementario;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use App\Jobs\ValidarSofiaJob;
 use App\Models\SofiaValidationProgress;
 
