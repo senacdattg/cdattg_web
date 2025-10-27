944c77c8baa45e931b434ca13f720df48d8158f9
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2025-10-27 09:00:09 -0500

    Cambio de datos hardcodeados a datos reales en estadísticas complementarios

--- routes/complementarios/estadisticas.php
+++ routes/complementarios/estadisticas.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\ComplementarioController;
 Route::get('/estadisticas', [ComplementarioController::class, 'estadisticas'])
     ->name('estadisticas')
     ->middleware('auth');
+
+Route::get('/estadisticas/api', [ComplementarioController::class, 'apiEstadisticas'])
+    ->name('estadisticas.api')
+    ->middleware('auth');
