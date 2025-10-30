8746e04024244cb0540396917ee110c79d3baa63
Samuel <rene28609@gmail.com>
2025-10-30 12:42:38 -0500

    arreglo de estylos y arreglos visuales

--- resources/views/complementarios/ver_programa_publico.blade.php
+++ resources/views/complementarios/ver_programa_publico.blade.php
@@ -1,24 +1,36 @@
 @extends('layout.master-layout-registro')
+@extends('layout.alertas')
+@section('css')
+    @vite(['resources/css/formulario_inscripcion.css'])
+@endsection
 @section('content')
-    <div class="card card-primary">
-        <div class="card-header">
-            <h3 class="card-title">
-                <i class="fas fa-graduation-cap mr-2"></i>{{ $programaData['nombre'] }}
-            </h3>
-            <div class="card-tools">
-                <a href="{{ route('programas-complementarios.publicos') }}" class="btn btn-outline-secondary btn-sm">
-                    <i class="fas fa-arrow-left mr-1"></i> Volver a Programas
-                </a>
-            </div>
-        </div>
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="text-center mb-5">
-                        <i class="{{ $programaData['icono'] }} fa-5x text-primary mb-4"></i>
-                        <h2 class="mb-3">{{ $programaData['nombre'] }}</h2>
-                        <span class="badge badge-success">Con Oferta</span>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-10 col-xl-8">
+                <div class="text-center mb-4">
+                    <img src="{{ asset('vendor/adminlte/dist/img/LogoSena.png') }}" alt="SENA Logo" class="mb-3" style="height: 80px;">
+                    <h2 class="text-success">Servicio Nacional de Aprendizaje - SENA</h2>
+                    <p class="text-muted">Regional Guaviare</p>
+                </div>
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <i class="fas fa-graduation-cap mr-2"></i>{{ $programaData['nombre'] }}
+                        </h3>
+                        <div class="card-tools">
+                            <a href="{{ route('programas-complementarios.publicos') }}" class="btn btn-outline-secondary btn-sm">
+                                <i class="fas fa-arrow-left mr-1"></i> Volver a Programas
+                            </a>
+                        </div>
                     </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="text-center mb-5">
+                                    <i class="{{ $programaData['icono'] }} fa-5x text-success mb-4"></i>
+                                    <h2 class="mb-3">{{ $programaData['nombre'] }}</h2>
+                                    <span class="badge badge-success">Con Oferta</span>
+                                </div>
 
                     <h5 class="mb-3">Descripción</h5>
                     <p class="text-muted mb-5">{{ $programaData['descripcion'] }}</p>
@@ -35,22 +47,25 @@
                     </div>
                 </div>
 
-                <div class="col-md-4">
-                    <div class="card card-widget widget-user">
-                        <div class="widget-user-header bg-info">
-                            <h3 class="widget-user-username">Inscripción</h3>
-                            <h5 class="widget-user-desc">Programa Disponible</h5>
-                        </div>
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <div class="description-block">
-                                        <span class="description-text">¿ESTÁS INTERESADO?</span>
-                                        <p class="text-muted mb-3">Realiza tu inscripción ahora mismo</p>
-                                        <a href="{{ route('programas-complementarios.inscripcion', $programaData['id'] ?? '') }}"
-                                            class="btn btn-primary btn-block">
-                                            <i class="fas fa-user-plus mr-1"></i> Inscribirse
-                                        </a>
+                            <div class="col-md-4">
+                                <div class="card card-widget widget-user">
+                                    <div class="widget-user-header bg-success">
+                                        <h3 class="widget-user-username">Inscripción</h3>
+                                        <h5 class="widget-user-desc">Programa Disponible</h5>
+                                    </div>
+                                    <div class="card-footer">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <div class="description-block">
+                                                    <span class="description-text">¿ESTÁS INTERESADO?</span>
+                                                    <p class="text-muted mb-3">Realiza tu inscripción ahora mismo</p>
+                                                    <a href="{{ route('programas-complementarios.inscripcion', $programaData['id'] ?? '') }}"
+                                                        class="btn btn-success btn-block">
+                                                        <i class="fas fa-user-plus mr-1"></i> Inscribirse
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -60,4 +75,5 @@
             </div>
         </div>
     </div>
+@include('layout.footer')
 @endsection
