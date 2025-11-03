78b94b1d6ff605331fae0090acd4d6f6e810a1ad
Samuel <rene28609@gmail.com>
2025-11-03 13:37:08 -0500

    reorganizacion de archivos y carpetas para mejora visual y escalabilidad para el desarrollo futuro, con el llamado de rutas arreglado y funcional

--- config/adminlte.php
+++ config/adminlte.php
@@ -743,7 +743,7 @@ return [
             'can' => 'VER PROGRAMA DE CARACTERIZACION',
             'submenu' => [
                 [
-                    'text' => 'Gestión de programas',
+                    'text' => 'Gestión complementarios',
                     'icon' => 'fas fa-graduation-cap',
                     'classes' => 'text-sm',
                     'submenu' => [
