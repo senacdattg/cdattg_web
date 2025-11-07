b1798519f9290054f74863f32e61f2a16b7f4b5f
Jeferson Alvarez <mymotre213q@gmail.com>
2025-11-07 10:07:51 -0500

    Refactor routes to enhance permission handling and legacy support. Updated 'evidencias' and 'asistencia' routes to exclude certain actions and added middleware for permission checks. Renamed legacy routes for 'caracterizacion' and 'fichaCaracterizacion' to maintain compatibility with the previous system. Adjusted 'jornada' and 'instructor' routes to reflect legacy naming conventions. Removed redundant resource definitions for 'programa'.

--- routes/caracterizacion/web_caracterizacion.php
+++ routes/caracterizacion/web_caracterizacion.php
@@ -8,11 +8,11 @@ Route::resource('caracterizacion', CaracterizacionController::class);
 route::middleware('can:VER PROGRAMA DE CARACTERIZACION')->group(function () {
     Route::get('/caracter/index', [CaracterizacionController::class, 'index'])->name('caracter.index');
     Route::get('/caracterizacion/search', [CaracterizacionController::class, 'show'])->name('caracterizacion.search');
-    Route::get('/caracterizacion/create', [CaracterizacionController::class, 'create'])->name('caracterizacion.create');
+    Route::get('/caracterizacion/create', [CaracterizacionController::class, 'create'])->name('caracterizacion.legacy.create');
     Route::post('/caracterizacion/ficha', [CaracterizacionController::class, 'getCaracterByFicha'])->name('caracterizacion.ficha');
-    Route::post('/caracterizacion/store', [CaracterizacionController::class, 'store'])->name('caracterizacion.store');
-    Route::get('/caracterizacion/{id}/edit', [CaracterizacionController::class, 'edit'])->name('caracterizacion.edit');
-    Route::get('/caracterizacion/destroy/{id}', [CaracterizacionController::class, 'destroy'])->name('caracterizacion.destroy');
-    Route::post('/caracterizacion/{id}', [CaracterizacionController::class, 'update'])->name('caracterizacion.update');
+    Route::post('/caracterizacion/store', [CaracterizacionController::class, 'store'])->name('caracterizacion.legacy.store');
+    Route::get('/caracterizacion/{id}/edit', [CaracterizacionController::class, 'edit'])->name('caracterizacion.legacy.edit');
+    Route::get('/caracterizacion/destroy/{id}', [CaracterizacionController::class, 'destroy'])->name('caracterizacion.legacy.destroy');
+    Route::post('/caracterizacion/{id}', [CaracterizacionController::class, 'update'])->name('caracterizacion.legacy.update');
     //Route::get('/caracterizacion/{id}/show', [CaracterizacionController::class, 'show'])->name('caracterizacion.show');
 });
--- routes/caracterizacion/web_fichas_caracterizacion.php
+++ routes/caracterizacion/web_fichas_caracterizacion.php
@@ -126,14 +126,4 @@ Route::middleware(['auth'])->group(function () {
             ->name('fichaCaracterizacion.importar');
     });
 
-    // Rutas de compatibilidad con el sistema anterior (mantener por ahora)
-    Route::middleware('can:VER FICHA CARACTERIZACION')->group(function () {
-        Route::get('/ficha/index', [FichaCaracterizacionController::class, 'index'])->name('ficha.index');
-        Route::get('/ficha/search', [FichaCaracterizacionController::class, 'search'])->name('ficha.search');
-        Route::get('/fichaCaracterizacion/create', [FichaCaracterizacionController::class, 'create'])->name('fichaCaracterizacion.create');
-        Route::post('/fichaCaracterizacion/store', [FichaCaracterizacionController::class, 'store'])->name('fichaCaracterizacion.store');
-        Route::get('/fichaCaracterizacion/{id}/edit', [FichaCaracterizacionController::class, 'edit'])->name('ficha.edit');
-        Route::post('/fichaCaracterizacion/{id}', [FichaCaracterizacionController::class, 'update'])->name('ficha.update');
-        Route::delete('/fichaCaracterizacion/{id}', [FichaCaracterizacionController::class, 'destroy'])->name('ficha.destroy');
-    });
 });
