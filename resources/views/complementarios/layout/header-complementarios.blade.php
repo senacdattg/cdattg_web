988b0bf13c2bbb66c29d05158aaf7b5e52d9e163
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2025-11-07 10:02:25 -0500

    Funcionalidad de menú desplegable para movil en ruta raiz

--- resources/views/complementarios/layout/header-complementarios.blade.php
+++ resources/views/complementarios/layout/header-complementarios.blade.php
@@ -7,7 +7,7 @@
             <span class="d-lg-none"><strong>SENA</strong></span>
         </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
