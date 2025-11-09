7c64a65f8ff4c8f18feb2f945f84c91b54a5514a
Deyson Urrego <deysonurrego159@gmail.com>
2025-11-09 17:31:43 -0500

    Corregiendo busgs en vistas

--- resources/views/inventario/_components/page-header.blade.php
+++ resources/views/inventario/_components/page-header.blade.php
@@ -32,21 +32,30 @@
             @if($showSearch)
                 <div class="search-box">
                     <i class="fas fa-search search-icon"></i>
-                    <input type="text" 
-                           id="searchInput" 
-                           class="search-input" 
-                           placeholder="{{ $searchPlaceholder }}" 
-                           autocomplete="off">
-                    <button type="button" class="search-clear" id="clearSearch" style="display: none;">
+                    <input 
+                        type="text" 
+                        id="searchInput" 
+                        class="search-input" 
+                        placeholder="{{ $searchPlaceholder }}" 
+                        autocomplete="off"
+                    >
+                    <button 
+                        type="button" 
+                        class="search-clear" 
+                        id="clearSearch" 
+                        style="display: none;"
+                    >
                         <i class="fas fa-times"></i>
                     </button>
                 </div>
             @endif
             
             @if($showCart)
-                <a href="{{ route('inventario.carrito.index') }}" 
-                   class="btn-lg carrito-icon-btn" 
-                   title="Ver Carrito">
+                <a 
+                    href="{{ route('inventario.carrito.index') }}" 
+                    class="btn-lg carrito-icon-btn" 
+                    title="Ver Carrito"
+                >
                     <i class="fas fa-shopping-cart"></i>
                     <span class="cart-badge" id="cartCount" style="display: none;">0</span>
                 </a>
@@ -66,29 +75,54 @@
 <div>
     @if($searchPlaceholder)
         <div class="mt-3 text-right">
-            <button class="btn btn-secondary btn-lg" data-toggle="modal" data-target="#modalEscanear">
+            <button 
+                class="btn btn-secondary btn-lg" 
+                data-toggle="modal" 
+                data-target="#modalEscanear"
+            >
                 <i class="fas fa-barcode"></i> Escanear
             </button>
         </div>
     @endif
 </div>
 
-<div class="modal fade" id="modalEscanear" tabindex="-1" role="dialog" aria-labelledby="modalEscanearLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+<div 
+    class="modal fade" 
+    id="modalEscanear" 
+    tabindex="-1" 
+    role="dialog" 
+    aria-labelledby="modalEscanearLabel" 
+    aria-hidden="true"
+>
+    <div 
+        class="modal-dialog modal-dialog-centered" 
+        role="document"
+    >
         <div class="modal-content">
             <div class="modal-header bg-dark text-white">
                 <h5 class="modal-title" id="modalEscanearLabel">
                     <i class="fas fa-barcode"></i> Escanear Código de Barras
                 </h5>
-                <button type="button" class="close text-white" data-dismiss="modal" aria-label="Cerrar">
+                <button 
+                    type="button" 
+                    class="close text-white" 
+                    data-dismiss="modal" 
+                    aria-label="Cerrar"
+                >
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
 
             <div class="modal-body text-center">
                 <p>Escanea el código de barras del producto usando el lector.</p>
-                <input type="text" id="inputCodigoBarras" class="form-control form-control-lg text-center" 
-                       placeholder="Esperando código..." autocomplete="off" autofocus>
+                <input 
+                    type="text" 
+                    id="inputCodigoBarras" 
+                    class="form-control form-control-lg text-center" 
+                    placeholder="Esperando código..." 
+                    autocomplete="off" 
+                    autofocus
+                >
                 <div id="resultadoBusqueda" class="mt-3"></div>
             </div>
         </div>
